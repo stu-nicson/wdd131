bd7ba53e9b51f526d822f1ed14c6605314006618
stu-nicson <whi19048@byui.edu>
2026-01-18 00:06:27 -0700

    mission statement done

--- mission/mission.js
+++ mission/mission.js
@@ -1,6 +1,7 @@
 
 let selectElem = document.querySelector('select');
 let logo = document.querySelector('img');
+let content = document.getElementById("content");
 
 selectElem.addEventListener('change', changeTheme);
 
@@ -8,8 +9,19 @@ function changeTheme() {
     let current = selectElem.value;
     if (current == 'dark') {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = "#3a3a3a";
+        document.body.style.color = "#ffffffff";
+        content.style.color = "white";
+        logo.src = "BYUI-Logo-White.png";
+
     } else {
         // code for changes to colors and logo
+        document.body.style.backgroundColor = "#ffff";
+        document.body.style.Color = "#000000ff";
+        content.style.color = "black";
+        logo.src = "byui-logo-blue.webp";
+
+
     }
 }           
                     
